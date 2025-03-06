e2f86da1e32afeffb919f8d65d47bf8987b2fbd5
Matthieu Rethers <matt.rethers@grafana.com>
2025-03-06 11:21:12 -0500

    Update repository URL to https://github.com/mrethers/grafana-stack-argo-local.git

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -14,9 +14,14 @@ git add ..
 git commit -m "Update repository URL to $REPO_URL"
 git push
 
+# create the namespaces
+kubectl apply -f ../namespaces
+
+# generate random passwords
 PASSWORD=$(head -c 16 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9')
 MIMIRS3KEY=$(head -c 16 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9')
 
+# create initial secrets
 kubectl create secret generic admin-user \
     --from-literal=adminUser=admin \
     --from-literal=adminPassword='$(PASSWORD)' \
@@ -26,4 +31,5 @@ kubectl create secret generic mimir-bucket-secret \
     --from-literal=MIMIRS3KEY='$(MIMIRS3KEY)' \
     -n loki
 
+# start app of apps
 kubectl apply -f root-app.yaml
